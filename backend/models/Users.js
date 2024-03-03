b6638c3881fe82fad46227d5884d18d1b91c1bf4
Rajat <saxena1975sanjeev@gmail.com>
2024-03-03 14:37:26 +0530

    initialized register form

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema({
-    name: {
+    email: {
         type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    roles: {
+        type: [String],
     }
 })
 
