fdcca68c080afdac28d73c360f6f49e73412df63
Rajat <saxena1975sanjeev@gmail.com>
2024-03-09 22:40:03 +0530

    added logout and user account in navbar

--- client/src/redux/actions/actionTypes.js
+++ client/src/redux/actions/actionTypes.js
@@ -1,4 +1,5 @@
 export const actionTypes = {
     SET_USER_DATA: 'SET_USER_DATA',
+    CLEAR_USER_DATA: 'CLEAR_USER_DATA',
     SET_LOGIN: 'SET_LOGIN',
 }
--- client/src/redux/reducers/authReducers.js
+++ client/src/redux/reducers/authReducers.js
@@ -12,6 +12,8 @@ export const authReducers = (state = initialState, action) => {
     switch(action.type) {
         case actionTypes.SET_USER_DATA:
             return {...state, userData: action.payload}
+        case actionTypes.CLEAR_USER_DATA:
+            return {...state, userData: initialState.userData}
         case actionTypes.SET_LOGIN:
             return {...state, isLogin: action.payload}
         default:
